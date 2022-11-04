0b7a4f3d9fe143709bf4fb67bfeab9144fece463
sampathpokuri <sampath.pokuri2@gmail.com>
2022-11-05 00:12:05 +0530

    update

---
+++ src/app/doctor/doctor.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { DocserveService } from '../docserve.service';
+import { PatserveService } from '../patserve.service';
+
+@Component({
+  selector: 'app-doctor',
+  templateUrl: './doctor.component.html',
+  styleUrls: ['./doctor.component.css']
+})
+export class DoctorComponent implements OnInit {
+doctor:any;
+var:any;
+rtd:any;
+obj:any;
+x:any;
+
+
+  constructor(private service : DocserveService,private serve : PatserveService) {
+    this.doctor=[{
+      docid:"",name:"",email:"",specialization:"",typeid:"",experience:""
+    }]
+    
+   }
+
+  ngOnInit(): void {
+ this.rtd=localStorage.getItem('customer');
+ this.var=localStorage.getItem('object');
+ this.obj=JSON.parse(this.var);
+ 
+ console.log('typeid:',this.rtd)
+ console.log('obj:',this.obj);
+//  console.log('id:',this.x)
+ this.service.getdoctorbytypeid(this.rtd).subscribe((result: any) => {this.doctor= result;});
+this.obj.docid=this.rtd;
+// this.serve.updatecustomer(this.obj).subscribe((result: any) => {console.log(result)})
+
+
+//  console.log('i am x',this.doctor)
+
+  }
+  log(docid:any){
+    
+    console.log('hii',docid);
+    this.obj.docid=docid
+    console.log(this.obj)
+    this.serve.senddocid(docid)
+    this.serve.updatecustomer(this.obj).subscribe((result: any) => {console.log(result)});
+
+  }
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PatserveService } from '../patserve.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+customer:any;
+loginForm:any;
+temp:any;
+allvar:any;
+  constructor(private service:PatserveService,private router : Router) {
+    this.loginForm={
+      email:'',
+      password:''
+    }
+    this.allvar=[{
+      email:'',
+      password:''
+    }]
+   }
+   logForm:any='';
+  ngOnInit(): void {
+   
+    this.service.getAllcustomers().subscribe((result: any) => {this.allvar=result,console.log(this.allvar)});
+   
+    
+   
+
+  }
+
+  log(logForm:any){
+    this.temp=0;
+    console.log(this.allvar)
+    for(const i in this.allvar){
+      // console.log(this.allvar[i].email,'  ',this.allvar[i].password)
+      this.temp=this.temp+1;
+      if(this.allvar[i].email==logForm.email && this.allvar[i].password==logForm.password){
+       this.loginForm=this.allvar[i];
+       this.temp=this.temp-1;
+        console.log('login succesfull',this.loginForm)
+        this.service.setUserLoggedIn();
+         localStorage.setItem("object", JSON. stringify(this.loginForm));
+         this.router.navigate(['cart']);
+         break;
+      }
+      
+      
+    }
+    console.log(this.temp,'  ',this.allvar.length)
+   if(this.temp==this.allvar.length){
+    alert('invalid credentials')
+   }
+
+
+//     this.service.getcustomerbyemail(logForm.email,logForm.password).subscribe((result: any) => {this.loginForm=result});
+
+
+// console.log('email:',this.loginForm.email,'password:',this.loginForm.password)
+// this.service.getcustomerbyemail(logForm.email,logForm.password).subscribe((result: any) => {this.loginForm=result});
+
+
+// console.log('email:',this.loginForm.email,'password:',this.loginForm.password)
+// if(this.loginForm.email!='' && this.loginForm.password !=''){
+  
+//   console.log('login succesfull',this.loginForm)
+//   localStorage.setItem("object", JSON. stringify(this.loginForm));
+  
+//   this.router.navigate(['cart']);
+// }
+
+//   else{
+//     this.service.getcustomerbyemail(logForm.email,logForm.password).subscribe((result: any) => {this.loginForm=result});
+
+// console.log('email:',this.loginForm.email,'password:',this.loginForm.password)
+// if(this.loginForm.email!='' && this.loginForm.password !=''){
+  
+//   console.log('login succesfull',this.loginForm)
+//   localStorage.setItem("object", JSON. stringify(this.loginForm));
+  
+//   this.router.navigate(['cart']);
+// }
+// else{
+//   alert('failed')
+//   console.log("failed")
+// }
+//   }
+
+   
+
+  }
+  
+  }
+
+
